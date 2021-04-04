c65ee1ea06e27e5a0d35136a98774d140267c902
Mike Mwanje <mwanjemike767@gmail.com>
2021-04-04 15:26:20 +0300

    Submit form values

--- src/sections/contact/index.js
+++ src/sections/contact/index.js
@@ -56,7 +56,7 @@ class Contact extends React.Component {
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
         body: this.encode({
           'form-name': e.target.getAttribute('name'),
-          ...name,
+          ...this.state,
         }),
       })
         .then(() => {
